80d85949436b1ee65cd77f88119a654ea4c65a2b
JoshNockOptum <jnock@alifemedical.com>
2015-07-02 14:42:10 -0700

    added mongo repository and connections

---
+++ VigilantIIS.WebDashboard/Repository/IMongoDbRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VigilantIIS.WebDashboard.Models;
+
+namespace VigilantIIS.WebDashboard.Repository
+{
+    public interface IMongoDbRepository
+    {
+        Task<IEnumerable<WebRequestModel>> GetAllRecords();
+        WebRequestModel GetEntityById(string id);
+        void Add(WebRequestModel entity);
+        void Update(string objectId, WebRequestModel entity);
+        void Delete(string objectId);         
+    }
+}
