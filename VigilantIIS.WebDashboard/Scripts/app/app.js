ed3b805eccdf80187e1616cfec548985f5650f56
JoshNockOptum <jnock@alifemedical.com>
2015-07-22 14:41:01 -0700

    Service complete but fails to inject into controller

--- VigilantIIS.WebDashboard/Scripts/app/app.js
+++ VigilantIIS.WebDashboard/Scripts/app/app.js
@@ -1,14 +1,26 @@
 ﻿/// <reference path="../angular.js" />
-
+"use strict";
+var testdata;
 var vigilApp = angular.module('vigilApp', []);
 
-vigilApp.controller('WelcomeController', function ($scope) {
+vigilApp.service('RequestsService', function ($q, $http, $rootScope, $timeout) {
+    var svc = this;
+    svc.GetRequests = function() {
+        $http.get("api/Requests").success(function (data, status, headers, config) {
+            return data;
+        }).error(function (data, status, headers, config) {
+            alert("there was an error requesting data from server: " + status);
+            return {};
+        });
+    };
+});
+
+vigilApp.controller('WelcomeController', ["RequestsService", function ($scope, RequestsService) {
 
     $scope.message = 'You are all going to die today.';
-    $scope.requests = {};
+    $scope.requests = RequestsService.GetRequests();
+    testdata = RequestsService.GetRequests();
+}]);
 
-});
 
-vigilApp.service('RequestsService', function() {
-    //todo: finish service
-});
+
